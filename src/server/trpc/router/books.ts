4a53b1d897ea18697a3d585c98d8c94253fa64b7
mike korakakis <mike.korakakis@gmail.com>
2022-12-16 16:19:14 +0200

    finished with categories

--- src/server/trpc/router/books.ts
+++ src/server/trpc/router/books.ts
@@ -1,7 +1,7 @@
 import { router, publicProcedure, protectedProcedure } from "../trpc";
 import { getAllSchema } from "@/types/zod/general";
 import {
-  deleteByFolderIdSchema,
+  deleteByLibraryIdSchema,
   getAllWithParamsSchema,
   transferBooksToFolderSchema,
   updateManySchema,
@@ -9,9 +9,8 @@ import {
 } from "@/types/zod/book";
 import { generateFilterParams } from "@/utils/generateFilterParams";
 import { FOLDER_ROOT } from "@/config";
-import { Book } from "@prisma/client";
 
-const entity = "book"
+const entity = "book";
 
 export const bookRouter = router({
   getAll: publicProcedure.input(getAllSchema).query(async ({ input, ctx }) => {
@@ -44,16 +43,16 @@ export const bookRouter = router({
       const bookCount = await ctx.prisma[entity].count({
         where: {
           library: { name: library },
-          folder: { name: folder},
-          subFolder: { name: subFolder},
+          folder: { name: folder },
+          subFolder: { name: subFolder },
           AND: colFilters,
         },
       });
       const books = await ctx.prisma[entity].findMany({
         where: {
-            library: { name: library },
-            folder: { name: folder},
-            subFolder: { name: subFolder},
+          library: { name: library },
+          folder: { name: folder },
+          subFolder: { name: subFolder },
           AND: colFilters,
         },
         skip: input.pageIndex * input.pageSize,
@@ -65,7 +64,6 @@ export const bookRouter = router({
       return { data: books, pageCount, bookCount };
     }),
 
-  
   update: publicProcedure
     .input(updateSchema)
     .mutation(async ({ input, ctx }) => {
@@ -88,73 +86,73 @@ export const bookRouter = router({
       );
     }),
 
-  tranferToOtherFolder: protectedProcedure
-    .input(transferBooksToFolderSchema)
-    .mutation(async ({ input, ctx }) => {
-      const books = await ctx.prisma[entity].findMany({
-        where: { folder: { id: input.fromId } },
-      });
-
-      const folder = await ctx.prisma.folder.findUnique({
-        where: { id: input.toId },
-        select: { name: true, id: true },
-      });
-
-      if (!folder) {
-        throw new Error("Folder not found");
-      }
-      const updatedBooks = books.map((book) => {
-        if (book && book?.fileUrl) {
-          const oldFolderName =
-            book &&
-            book?.fileUrl &&
-            book?.fileUrl.replace(FOLDER_ROOT + "/", "").split("/")[0];
-
-          if (oldFolderName)
-            return {
-              ...book,
-              fileUrl: book?.fileUrl.replace(
-                `/${oldFolderName}/`,
-                `/${folder.name}/`
-              ),
-              folderId: folder.id,
-            };
-          return {
-            ...book,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            folderId: folder.id,
-          };
-        }
-      });
-      // filter out undefined values
-      const filteredBooks = updatedBooks.filter(
-        (book) => typeof book !== "undefined"
-      );
-      //update with loop
-      for (const book of filteredBooks) {
-        book &&
-          (await ctx.prisma[entity].update({
-            where: { id: book.id },
-            data: book,
-          }));
-      }
-      //update with prisma
-
-      //   await ctx.prisma[entity].updateMany({
-      //     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      //     //@ts-ignore
-      //     where: { id: { in: filteredBooks.map((book) => book.id) } },
-      //     data: updatedBooks,
-      //   });
-      return updatedBooks;
-    }),
-
-  deleteByFolderId: protectedProcedure
-    .input(deleteByFolderIdSchema)
+  deleteByLibraryId: protectedProcedure
+    .input(deleteByLibraryIdSchema)
     .mutation(async ({ input, ctx }) => {
       return ctx.prisma[entity].deleteMany({
-        where: { folderId: input.id },
+        where: { libraryId: input.id },
       });
     }),
 });
+
+//   tranferToOtherFolder: protectedProcedure
+//     .input(transferBooksToFolderSchema)
+//     .mutation(async ({ input, ctx }) => {
+//       const books = await ctx.prisma[entity].findMany({
+//         where: { folder: { id: input.fromId } },
+//       });
+
+//       const folder = await ctx.prisma.folder.findUnique({
+//         where: { id: input.toId },
+//         select: { name: true, id: true },
+//       });
+
+//       if (!folder) {
+//         throw new Error("Folder not found");
+//       }
+//       const updatedBooks = books.map((book) => {
+//         if (book && book?.fileUrl) {
+//           const oldFolderName =
+//             book &&
+//             book?.fileUrl &&
+//             book?.fileUrl.replace(FOLDER_ROOT + "/", "").split("/")[0];
+
+//           if (oldFolderName)
+//             return {
+//               ...book,
+//               fileUrl: book?.fileUrl.replace(
+//                 `/${oldFolderName}/`,
+//                 `/${folder.name}/`
+//               ),
+//               folderId: folder.id,
+//             };
+//           return {
+//             ...book,
+//             createdAt: new Date(),
+//             updatedAt: new Date(),
+//             folderId: folder.id,
+//           };
+//         }
+//       });
+//       // filter out undefined values
+//       const filteredBooks = updatedBooks.filter(
+//         (book) => typeof book !== "undefined"
+//       );
+//       //update with loop
+//       for (const book of filteredBooks) {
+//         book &&
+//           (await ctx.prisma[entity].update({
+//             where: { id: book.id },
+//             data: book,
+//           }));
+//       }
+//       //update with prisma
+
+//       //   await ctx.prisma[entity].updateMany({
+//       //     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//       //     //@ts-ignore
+//       //     where: { id: { in: filteredBooks.map((book) => book.id) } },
+//       //     data: updatedBooks,
+//       //   });
+//       return updatedBooks;
+//     }),
